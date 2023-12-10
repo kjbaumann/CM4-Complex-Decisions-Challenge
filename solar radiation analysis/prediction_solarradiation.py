551d49ae30eb9c5e29f991e363fdbd1ccabb4de9
Konrad Baumann <baumannkonrad@outlook.de>
2023-12-09 23:03:25 -0500

    implements ARIMA model

--- solar radiation analysis/prediction_solarradiation.py
+++ solar radiation analysis/prediction_solarradiation.py
@@ -6,6 +6,8 @@ from helper_functions import read_weather_data
 from datetime import datetime
 from statsmodels.tsa.stattools import adfuller
 import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import pmdarima as pm
 
 # Loads Weather data from 2012 to 2022 and drops columns that are not needed
 df = read_weather_data('./data/Papua New Guinea 2002-01-01 to 2022-12-31.csv')
@@ -29,35 +31,68 @@ df['totalsolarradiation'] = df['hoursofsunlight'] * df['solarradiation']
 df['date'] = pd.to_datetime(df['datetime'], format='%Y-%m-%d')
 
 # Drops all columns but totalsolarradiation and date
-df = df.drop(['solarradiation', 'sunrise', 'sunset', 'hoursofsunlight', 'datetime'], axis=1)
+df = df.drop(['solarradiation', 'sunrise', 'sunset', 'hoursofsunlight', 'datetime', 'date'], axis=1)
 
 # Sets date column as index
-df.set_index('date', inplace=True)
+# df.set_index('date', inplace=True)
 
-# Testing for stationary data
-test_results = adfuller(df['totalsolarradiation'])
+# Plots dataframe for initial analysisdf.plot()
+plt.show()
+df.plot()
+plt.show()
 
-# Null Hypothesis: It is non stationary
-# Alternative Hypothesis. It is stationary
-# Significance Level: 0.05
+# Transforms the data
+# df['totalsolarradiation'] = np.log(df['totalsolarradiation'])
 
-def adfuller_test(totalsolarradiation):
-    result = adfuller(totalsolarradiation)
-    labels = ['ADF Test Statistic', 'p_value', '#Lags Used', 'Number of Observations Used']
-    for value, label in zip(result, labels):
-        print(label + ' : ' + str(value))
-    if result[1] < 0.05:
-        print("P value is less than 0.05 that means we can reject the null hypothesis(Ho). Therefore we can conclude that data has no unit root and is stationary")
-    else:
-        print("Weak evidence against null hypothesis that means time series has a unit root which indicates that it is non-stationary ")
+# Plots dataframe for analysis
+df.plot()
+plt.show()
 
-adfuller_test(df['totalsolarradiation'])
+# Splits the dataframe into test and train
+msk = (df.index < len(df)-365)
 
-# Fits Model
-model = ARIMA(df['totalsolarradiation'], order=(1,1,1))
-results = model.fit()
+df_train = df[msk].copy()
+df_test = df[~msk].copy()
 
-df['forecast'] = results.predict(start='2023-01-01',end='2023-12-31',dynamic=True)
+# Checks if data is stantionary
+adf_test = adfuller(df_train)
+print(f'p-value: {adf_test[1]}')
 
-print(results.summary())
-print(df['forecast'])
+# Selecting p and q parameters for the ARIMA model
+acf_diff = plot_acf(df_train)
+plt.show()
+pacf_diff = plot_pacf(df_train)
+plt.show()
+
+# Model 1: (2,1,0) parameters
+model = ARIMA(df_train, order=(2,1,0))
+model_fit = model.fit()
+print(model_fit.summary())
+
+# Residual Analysis
+residuals = model_fit.resid[1:]
+fig, ax = plt.subplots(1,2)
+residuals.plot(title='Residuals', ax=ax[0])
+residuals.plot(title='Density', kind='kde', ax=ax[1])
+plt.show()
+
+acf_res = plot_acf(residuals)
+pacf_rea = plot_pacf(residuals)
+plt.show()
+
+# Forecast with test data
+forecast_test = model_fit.forecast(len(df_test))
+df['forecast_manual'] = [None]*len(df_train) + list(forecast_test)
+df.plot()
+plt.show()
+
+# Model 2: Auto-fit with pmdarima
+auto_arima = pm.auto_arima(df_train, stepwise=False, seasonal=False)
+print(auto_arima)
+print(auto_arima.summary())
+
+# Forecast with test data
+forecast_test_auto = auto_arima.predict(n_periods=len(df_test))
+df['forecast_auto'] = [None]*len(df_train) + list(forecast_test_auto)
+df.plot()
+plt.show()
